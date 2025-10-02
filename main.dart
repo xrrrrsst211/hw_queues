5fc5a0b9778e1fe47b44c7d473fd73aa641b92c5
xrrrrsst211 <2023002@miu.edu.mn>
2025-10-02 09:04:47 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,11 +2,15 @@ import 'queue_list.dart';
 import 'queue_linkedlist.dart';
 
 void main() {
-  var q1 = QueueList()
-    ..enqueue(10)..enqueue(20);
-  print("QueueList: ${q1.dequeue()}");
 
-  var q2 = QueueLinkedList()
-    ..enqueue(30)..enqueue(40);
-  print("QueueLinkedList: ${q2.dequeue()}");
+  var q1 = QueueList();
+  q1.enqueue(10);
+  q1.enqueue(20);
+  print("QueueList dequeue: ${q1.dequeue()}"); // 10
+
+
+  var q2 = QueueLinkedList();
+  q2.enqueue(30);
+  q2.enqueue(40);
+  print("QueueLinkedList dequeue: ${q2.dequeue()}"); // 30
 }
